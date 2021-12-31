3de2f0e869b00456c68d0c80b12121093108b4f3
Renan Marques <henrenan@gmail.com>
2021-12-30 23:50:33 -0300

    embaralhar

--- script.js
+++ script.js
@@ -3,40 +3,25 @@ const BACK = "card_back"
 const CARD = "card"
 const ICON = "icon"
 
-let techs = [
-    'bootstrap',
-    'css',
-    'electron',
-    'firebese',
-    'html',
-    'javascript',
-    'jquery',
-    'mongo',
-    'node',
-    'react'];
-
-        let cards = null;
 
         startGame();
 
-        function startGame () {
-             cards = createcardsFromTechs(techs);
-             shuffleCards(cards)
-               
-             initializeCards(cards);
+        function startGame() {
+             
+             initializeCards(game.createCardsFromTechs());
         }
 
         function initializeCards(cards) {
             let gameBoard = document.getElementById("gameBoard")
             
-            cards.forEach(card=>{
+            game.cards.forEach(card=> {
 
                 let cardElement = document.createElement('div');
                 cardElement.id = card.id;
                 cardElement.classList.add(CARD); 
                 cardElement.dataset.icon = card.icon;
 
-                createCardContent(card, cardElement)
+                createCardContent(card, cardElement); 
 
                 cardElement.addEventListener('click', flipCard)
                 gameBoard.appendChild(cardElement);
@@ -45,72 +30,31 @@ let techs = [
 
         function createCardContent(card, cardElement){
 
-            createCardFace(FRONT, card, cardElement)
-            createCardFace(BACK, card, cardElement)
+            createCardFace(FRONT, card, cardElement);
+            createCardFace(BACK, card, cardElement);
 
         }
 
         function createCardFace(face, card, element) {
+
             let cardElementFace = document.createElement('div');
             cardElementFace.classList.add(face);
+
             if(face === FRONT){
                 let iconElement =  document.createElement('img');
                 iconElement.classList.add(ICON)
-                iconElement.src = "./imagens/" + card.icon + ".png"
+                iconElement.src = "./imagens/" + card.icon + ".png";
+                cardElementFace.appendChild(iconElement);
 
             }else{
                 cardElementFace.innerHTML = "&lt/&gt";
             }
+            element.appendChild(cardElementFace);
         }
         
 
+    function flipCard() {
 
-        function shuffleCards(cards){
-            let currentIndex = cards.length;
-            let randomIndex = 0;
-
-            while (currentIndex !== 0) {
-                randomIndex = Math.floor(Math.random() * currentIndex)
-                currentIndex--;
-
-                [cards[randomIndex], cards[currentIndex] = cards[currentIndex], cards[randomIndex]]
-
-            }
-        }
-
-
-    createcardsFromTechs(techs);
-    function createcardsFromTechs(techs) {
-
-        let cards = [];
-
-        techs.forEach((tech) =>{
-            cards.push(createPairFromTech(tech));
-        })
-        
-        return cards.flatMap(pair => pair);
-    }
-
-    function createPairFromTech(tech){
-
-        return [{
-            id:createIdWithTech(tech),
-            icon: tech,
-            flipped: false,
-        }, {
-            id:createIdWithTech(tech),
-            icon: tech,
-            flipped: false,
-        }]
-    }
-
-    function createIdWithTech(tech) {
-        return tech + parseInt(Math.random()*1000);
+        this.classList.add("flip")
 
     }
-
-    function flipCard(){
-
-    }
-
-    6,31
